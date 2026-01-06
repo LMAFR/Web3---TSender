c48ad1960105b9fe206897dbefca80a3c01c8185
algio <alejandrofloridoreyes@gmail.com>
2026-01-06 18:22:05 +0100

    Adapted next config file in order to be able to compile the project as a static site, so we could deploy it (for example, in Netlify or similar platforms).

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // It will be a static site socketClientCache...
+  output:"export",
+  images: {
+    unoptimized: true,
+  },
+  basePath:"",
+  assetPrefix:"./",
+  trailingSlash:true,
 };
 
 export default nextConfig;
